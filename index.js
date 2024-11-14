b2527f9e504c8530fd68bf7f2934b9dab5658d56
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2024-11-14 20:33:24 +0530

    configured vercel

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json());
+
 app.use('/api/event', require('./routes/single_event.js'));
 app.use('/api/events', require('./routes/events.js'));
 
 app.listen(PORT, () => {
     console.log("listening to port ", PORT);
 })
+
+module.exports = app;
