51e196e75da3c7f7c89b3e10c5dfe6c71515ff4a
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2024-11-19 17:30:34 +0530

    CORS config update

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with specific origins if needed
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 
 app.use('/api/event', require('./api/event.js'));
 app.use('/api/allevents', require('./api/allevents.js'));
